313088ca32d6533d8732900e5dcd7b66ddf03132
Owner <129130014+hysteria222@users.noreply.github.com>
2024-09-18 22:14:13 -0500

    we did it.

--- api/server.js
+++ api/server.js
@@ -4,25 +4,7 @@ const express = require('express');
 const User = require('./users/model');
 const server = express();
 server.use(express.json())
-server.post('/api/users', (req, res) => {
-    const user = req.body
-    if (!user.name || !user.bio) {
-        res.status(400).json({
-            message: "Please provide name and bio for the user",
-        })
-    } else {
-        User.insert(user)
-        .then(createdUser => {
-            res.status(201).json(createdUser)
-        })
-        .catch(err => { 
-            res.status(400).json({
-                message: "Error Creating New User",
-                err: err.message,
-            });
-        });
-    }
-})
+
 
 server.get('/api/users', (req, res) => { 
     User.find()
@@ -54,6 +36,70 @@ server.get('/api/users/:id', (req, res) => {
             })
         });
 });
+
+server.post('/api/users', (req, res) => {
+    const user = req.body
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user",
+        })
+    } else {
+        User.insert(user)
+        .then(createdUser => {
+            res.status(201).json(createdUser)
+        })
+        .catch(err => { 
+            res.status(400).json({
+                message: "Error Creating New User",
+                err: err.message,
+            });
+        });
+    }
+})
+
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist" 
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user" 
+                })
+            } else {
+                const updatedUser = await User.update(req.params.id, req.body)
+                res.status(200).json(updatedUser)
+            }
+        } 
+    } catch (err) {
+        res.status(400).json({
+            message: "Error Updating User",
+            err: err.message,
+        });
+    }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist" 
+            })
+        } else {
+            const deletedUser = await User.remove(possibleUser.id)
+            res.status(200).json(deletedUser)
+        }
+    } catch (err) {
+        res.status(400).json({
+            message: "Error Deleting User",
+            err: err.message,
+        });
+    }
+})
 
 server.use('*', (req, res) => {
     res.status(404).json({
